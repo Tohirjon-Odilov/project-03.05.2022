0211846bd2d961fbe6663b62d655c167e8429114
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-05-03 19:59:19 +0500

    yt-movie-app add navbar and img

---
+++ vue-project/vue-yt-movie-app/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+          alt="Naruto Poster"
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>
+            Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for
+            recognition and dreams of becoming the Hokage, the village's leader and
+            strongest ninja.
+          </p>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+    }
+  }
+}
+</style>
