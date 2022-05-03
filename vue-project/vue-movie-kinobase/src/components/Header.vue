aaf0f8baca4f46ccd307243037c954e0e5877af4
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-05-03 18:09:13 +0500

    add vue weather app and movie upgrade

---
+++ vue-project/vue-movie-kinobase/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-center">
+  <div class="container">
+    <router-link to="/" class="navbar-brand text-white">Kinobase</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Serials</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Cartoon</a>
+
+        <li class="nav-item">
+          <input type="search" placeholder="Search movie ..." class="form-control">
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name:"HeaderComponent"
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-project/vue-movie-kinobase/src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <Header />
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <div class="container">
+      <div class="d-flex justify-content-start p-2 mt-5 h3">Most Popular Movies</div>
+      <div class="row">
+        <div
+          class="col-sm-6 col-md-3 col-lg-4"
+          v-for="(movie, key) in movies"
+          v-bind:key="key"
+        >
+          <div class="movie">
+            <div class="movie-img">
+              <img src="../assets/img/poster.png" alt="" />
+            </div>
+            <div class="movie-info">
+              <a v-bind:href="movie.movie_imdb_link" class="title">
+                {{ movie.movie_title }}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+/* eslint-disable */
+import Header from "../components/Header.vue";
+const axios = require("axios").default;
+import data from "../assets/data.json";
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  components: {
+    Header,
+  },
+  mounted() {
+    let list = [];
+    for (let k in data) {
+      if (list.length > 20) {
+        break;
+      }
+      list.push(data[k]);
+    }
+    this.movies = list;
+    for (let i = 0; i < 20; i++) {
+      for (let k in data) {
+        console.log(data[i]);
+      }
+    }
+    axios.get("../assets/data.json", {}).then((data) => console.log(data));
+    fetch("../assets/data.json")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
+};
+</script>
