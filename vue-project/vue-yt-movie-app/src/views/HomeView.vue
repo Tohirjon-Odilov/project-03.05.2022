56f341441400137b7bf3a43e3b0bb52fba412bc4
Tohirjon-Odilov <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-05-04 14:29:11 +0500

    Myself omdbapi

--- vue-project/vue-yt-movie-app/src/views/HomeView.vue
+++ vue-project/vue-yt-movie-app/src/views/HomeView.vue
@@ -17,22 +17,130 @@
         </div>
       </router-link>
     </div>
+
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="search" />
+      <input type="submit" value="Search" />
+    </form>
+
+    <div class="movies-list">MOVIES</div>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import env from "@/env.js";
+
+export default {
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            movies.value = data.Search;
+            search.value = "";
+            console.log(movies.value);
+          });
+      }
+    };
+
+    return {
+      search,
+      movies,
+      SearchMovies,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 .home {
   .feature-card {
     position: relative;
+
     .featured-img {
       display: block;
       width: 100%;
       height: 300px;
       object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type="text"] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+
+        &:focus {
+          box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+
+      &[type="submit"] {
+        width: 100%;
+        max-width: 300px;
+        background-color: #42b883;
+        padding: 16px;
+        border-radius: 8px;
+        color: #fff;
+        font-size: 20px;
+        text-transform: uppercase;
+        transition: 0.4s;
+
+        &:active {
+          background-color: #3b8070;
+        }
+      }
     }
   }
 }
